a55f35ccfbbc446419b61fd86c50eea8cb31a164
Roger Jungemann <roger@thefifthcircuit.com>
2017-01-16 19:48:32 -0800

    Remove external scheme definitions

--- share/run-on-schemes.scm
+++ share/run-on-schemes.scm
@@ -16,49 +16,6 @@
         (chibi app)
         (chibi config))
 
-;; ----------------------------------------------------------------------------
-;; These functions are for determining the path to the scheme definitions file.
-;; ----------------------------------------------------------------------------
-
-(define (ps)
-  (string-split (first (process->output+error+status (string-append "ps -ef"))) #\newline))
-
-(define pid
-  (number->string (current-process-id)))
-
-(define (process-ps-line line done)
-  (let ((pieces (string-split (string-trim line))))
-    (if (> (length pieces) 0)
-      (let ((current-pid (second pieces)))
-        (if (and (> (string-length current-pid) 0) (equal? pid current-pid))
-          (done line))))))
-
-(define (current-process-line)
-  (call/cc
-    (lambda (return)
-      (for-each
-        (lambda (line)
-          (process-ps-line line return))
-        (ps)))))
-
-(define (script-path script-name)
-  (call/cc
-    (lambda (return)
-      (for-each
-        (lambda (n)
-          (define pieces (string-split n #\/))
-          (if (and (> (length pieces) 0) (equal? (last pieces) script-name))
-            (return n)))
-        (string-split (current-process-line))))))
-
-(define (script-directory script-name)
-  (path-directory (script-path script-name)))
-
-(define (default-schemes-path)
-  (string-append (script-directory "run-on-schemes.scm") "/" "default-schemes.scm"))
-
-;; ----------------------------------------------------------------------------
-
 (define (find-executable name)
   (let ((paths (string-split (get-environment-variable "PATH") #\:)))
     (let loop ((paths paths))
@@ -70,8 +27,6 @@
                      (file-is-executable? full-path))
                 full-path
                 (loop (cdr paths))))))))
-
-(define *schemes* (make-hash-table))
 
 (define-record-type scheme
   (make-scheme name executable-names make-compile make-run get-version)
@@ -113,6 +68,190 @@
   (lambda (executable input-filename compile-data)
     (execute-runner (show #f executable " " parameters " " input-filename))))
 
+(define *schemes* (make-hash-table))
+
+(define-scheme 'bigloo '("bigloo")
+  (lambda (bigloo input-filename)
+    (let-values (((output error status time) (execute-runner (string-append bigloo " " input-filename " -O6 -call/cc -copt -O3 -o /tmp/bigloo"))))
+      (values output error status time "/tmp/bigloo")))
+  (lambda (bigloo input-file executable)
+    (execute-runner executable))
+  (make-version-finder #f "Bigloo (" #\)))
+
+(define-scheme 'biwascheme '("biwas")
+  #f
+  #f
+  (make-version-finder "--version" " version " #f))
+
+(define-scheme 'bones '("bones")
+  (lambda (bones input-file)
+    (let-values (((bones-output bones-error bones-exit-status bones-time)
+                  (execute-runner (show #f bones " " input-file " -o /tmp/bones.s"))))
+      (if (zero? bones-exit-status)
+          (let-values (((nasm-output nasm-error nasm-exit-status nasm-time)
+                        (execute-runner (show #f "nasm -I/usr/share/bones/ -f elf64 /tmp/bones.s -o /tmp/bones.o"))))
+            (if (zero? nasm-exit-status)
+                (let-values (((gcc-output gcc-error gcc-exit-status gcc-time)
+                              (execute-runner (show #f "gcc /tmp/bones.o -o /tmp/bones"))))
+                  (values (string-append bones-output nasm-output gcc-output)
+                          (string-append bones-error nasm-output gcc-output)
+                          gcc-exit-status
+                          (+ bones-time nasm-time gcc-time)
+                          "/tmp/bones"))
+                (values (string-append bones-output nasm-output)
+                        (string-append bones-error nasm-error)
+                        nasm-exit-status
+                        (+ bones-time nasm-time)
+                        "/tmp/bones")))
+          (values bones-output bones-error bones-exit-status bones-time "/tmp/bones"))))
+  (lambda (bones-path input-file executable)
+    (execute-runner executable))
+  (make-version-finder "-v" "" #f))
+
+(define-scheme 'chez '("chez-scheme")
+  #f
+  (simple-parameter-runner "-q")
+  (lambda (executable scheme)
+    ;; read version from stderr
+    (string-trim (second (process->output+error+status (string-append executable " --version"))) #\newline)))
+
+(define-scheme 'chibi '("chibi-scheme")
+  #f
+  #f
+  (make-version-finder "-V" "chibi-scheme " #\space))
+
+(define-scheme 'chicken '("csc")
+  (lambda (executable input-filename)
+    (let-values (((output error status time) (execute-runner (string-append executable " -o /tmp/chicken " input-filename))))
+      (values output error status time "/tmp/chicken")))
+  (lambda (bones-path input-file executable)
+    (execute-runner executable))
+  (make-version-finder "-version" "Version " #\space))
+
+(define-scheme 'chicken-csi '("csi")
+  #f
+  (simple-parameter-runner "-q")
+  (make-version-finder "-version" "Version " #\space))
+
+(define-scheme 'cyclone '("cyclone")
+  #f
+  #f
+  (make-version-finder "-v" "Version " #\space))
+
+;; (define-scheme 'foment '("foment")
+;;   #f
+;;   #f
+;;   #f)
+
+(define-scheme 'gambitc '("gambitc")
+  (lambda (gambitc input-file)
+    (let-values (((output error status time) (execute-runner (show #f gambitc " -o /tmp/gambitc -exe " input-file))))
+      (values output error status time "/tmp/gambitc")))
+  (lambda (gambitc input-file executable)
+    (execute-runner executable))
+  (make-version-finder "-v" "" #\space))
+
+(define-scheme 'gauche '("gosh")
+  #f
+  #f
+  (make-version-finder "-V" ", version " #\space))
+
+(define-scheme 'guile '("guile")
+  #f
+  #f
+  (make-version-finder "--version" "guile (GNU Guile) " #\newline))
+
+(define-scheme 'ironscheme '("ironscheme")
+  #f
+  #f
+  (make-version-finder "-V" "" #\newline))
+
+(define-scheme 'kawa '("kawa")
+  #f
+  #f
+  (make-version-finder "--version" "Kawa " #\newline))
+
+(define-scheme 'larceny '("larceny")
+  #f
+  (simple-parameter-runner "--r7rs --program")
+  (make-version-finder "--version" "Larceny " #\space))
+
+(define-scheme 'mit '("mit-scheme")
+  #f
+  (simple-parameter-runner "--quiet --no-init-file --load")
+  (make-version-finder "--version" "  Release " #\space))
+
+(define-scheme 'mosh '("mosh-scheme")
+  #f
+  #f
+  #f) ;; outputs to stderr
+
+(define-scheme 'owl-lisp '("ol")
+  #f
+  #f
+  (make-version-finder "--version" "Owl Lisp " #\newline))
+
+;; (define-scheme 'petit-larceny '("larceny")
+;;   #f
+;;   (simple-parameter-runner "--r7rs --program")
+;;   (make-version-finder "--version" "Larceny " #\space))
+
+(define-scheme 'petite-chez '("petite")
+  #f
+  (simple-parameter-runner "-q")
+  (lambda (executable scheme)
+    (string-trim (second (process->output+error+status (string-append executable " --version"))) #\newline)))
+
+(define-scheme 'racket '("racket")
+  #f
+  #f
+  (lambda x (let ((str (apply (make-version-finder "--version" "Welcome to Racket v" #\newline) x)))
+         ;; strip end-of-sentence #\.
+         (substring str 0 (- (string-length str) 1)))))
+
+(define-scheme 'rscheme '("fshell")
+  #f
+  (simple-parameter-runner "-q")
+  (make-version-finder "--version" "" #\newline))
+
+;; (define-scheme 'rscheme-rsc '("rsc") #f) ;; compiling
+
+;; (define-scheme 'rhizome '("pisc") #f) ;; needs multiple executables ;-/
+
+(define-scheme 'sagittarius '("sagittarius")
+  #f
+  #f
+  (make-version-finder "--version" ", version " #\space))
+
+;; (define-scheme 'scheme48 '("scheme48") #f) ;; todo: fix problems with loading script files
+
+(define-scheme 'stalin-chicken '("chicken-stalin")
+  (lambda (executable input-filename)
+    ;; TODO: run through alexpander first
+    (let-values (((output error exit-status time)
+                  (execute-runner (string-append executable " -On -Ob -Om -Or -Ot -d -d1 -k -copt -O3 " input-filename))))
+      (values output error exit-status time (substring input-filename 0 (- (string-length input-filename) 4)))))
+  (lambda (stalin-path input-file executable)
+    (execute-runner executable))
+  (lambda (executable scheme)
+    "unknown"))
+
+(define-scheme 'tinyscheme '("tinyscheme")
+  #f
+  #f
+  ;; tinyscheme prints to stdout, so run with no parameters, no stdin will immediately terminate it after printing the version
+  (make-version-finder #f "TinyScheme " #f))
+
+(define-scheme 'vicare '("vicare")
+  #f
+  #f
+  (make-version-finder "--version" " version " #\,))
+
+(define-scheme 'ypsilon '("ypsilon")
+  #f
+  #f
+  (make-version-finder "--version" "Ypsilon " #\space))
+
 (define (find-schemes)
   "Return a list of (<executable-path> . <scheme record>). Path is #f if not found."
   (map (lambda (scheme)
@@ -136,12 +275,6 @@
          (default-schemes (conf-get config '(command run default-schemes)))
          (matching-schemes-raw (conf-get config '(command run scheme)))
          (matching-schemes (if matching-schemes-raw (map string-trim (string-split matching-schemes-raw #\,)) #f)))
-
-    ; Allow for a path to a .scm file be passed, containing a list of schemes to use.
-    (if (equal? default-schemes #f)
-      (load (default-schemes-path))
-      (load default-schemes))
-
     (parameterize ((ansi-escapes-enabled? (if no-color? #f (ansi-escapes-enabled?))))
       (if (null? arguments)
           (begin
@@ -217,8 +350,7 @@
      (or
       (list "List available Scheme implementations (-v for details)" (@ (verbose boolean (#\v))) (,list-schemes))
       (run "Run an input with multiple Scheme implementations."
-           (@ (default-schemes string (#\f) "Path to a file containing a list of scheme definitions to use")
-              (scheme string (#\i) "Include only matching Schemes, comma-separated list")
+           (@ (scheme string (#\i) "Include only matching Schemes, comma-separated list")
               (text boolean (#\t) "Output text, not s-expressions")
               (no-color boolean (#\c) "Disable color output"))
            (,run-schemes))
